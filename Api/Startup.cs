85d303155189a8efef3c99fae109f6d2e50d4b2f
Breno Baronte <brenophillip@gmail.com>
2021-02-16 11:35:56 -0300

    Updating obsolete host environment

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -17,6 +17,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 
 namespace Api
@@ -77,7 +78,7 @@ namespace Api
             return new AutofacServiceProvider(Container);
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
